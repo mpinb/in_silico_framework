b833be570354e1ae53cb3a734e241aca63972b0a
amirji <amir.najafgholi@gmail.com>
2019-11-08 02:05:43 +0100

    move the read() function in exTrMatrix.py to rw module and call it readTransformationMatrix

--- rw/rw.py
+++ rw/rw.py
@@ -587,3 +587,28 @@ class Rw:
             print(outputDataFile)
             print("in wirteResult()")
             return "error"
+
+
+    def readTransformationMatrix(self, file):
+        """
+        The function below can read the transformation matrix numbers written
+        in an am file
+
+        """
+        matrix = []
+        vector = []
+        row = []
+        with open(file, 'r') as f:
+            lines = f.readlines()
+            for idx, line in enumerate(lines):
+                if line.rfind("TransformationMatrix")>-1:
+                    matches = re.findall('-?\d+\.\d+|-?\d+', line)
+                    vector = map(float, matches)
+            for i in range(4):
+                for j in range(4):
+                    k = j + i*4
+                    row.append(vector[k])
+                matrix.append(row)
+                row = []
+
+        return matrix
--- transformTools/exTrMatrix.py
+++ transformTools/exTrMatrix.py
@@ -1,30 +1,4 @@
 import numpy as np
-import re
-
-
-def read(file):
-    """
-    The function below can read the transformation matrix numbers written
-    in an am file
-
-    """
-    matrix = []
-    vector = []
-    row = []
-    with open(file, 'r') as f:
-        lines = f.readlines()
-        for idx, line in enumerate(lines):
-            if line.rfind("TransformationMatrix")>-1:
-                matches = re.findall('-?\d+\.\d+|-?\d+', line)
-                vector = map(float, matches)
-        for i in range(4):
-            for j in range(4):
-                k = j + i*4
-                row.append(vector[k])
-            matrix.append(row)
-            row = []
-
-    return matrix
 
 
 def getTransformation(src, dst):
