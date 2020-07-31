76848b9b98c2c0b16df8b558da9d0f37b102070a
Amir Najafgholi <amir.najafgholi@gmail.com>
2020-07-31 17:04:27 +0200

    make data schems of the pipeline

--- thickness/Data.py
+++ thickness/Data.py
@@ -1,6 +1,16 @@
-class Data:
+class DataPatch:
+    def __init__(self, label=None, points=None, image=None):
+        self.label = label
+        self.points = points
+        self.image = image
 
-    def __init__(self):
-        self.data_inputs = []
-        self.data_in_progress = []
-        self.data_output = []
+
+class DataCollection:
+    def __init__(self, points=None):
+        self.points = points
+
+
+class DataPipeline:
+    def __init__(self, labels=None):
+        self.labels = labels
+
