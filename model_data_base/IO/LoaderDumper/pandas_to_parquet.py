b58e63d7d49e4c98ab491af9a2c13bbd9a31eb2b
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-10-23 14:20:47 +0200

    str column names for parquet

--- model_data_base/IO/LoaderDumper/pandas_to_parquet.py
+++ model_data_base/IO/LoaderDumper/pandas_to_parquet.py
@@ -3,6 +3,8 @@ import os
 import compatibility
 import pandas as pd
 from . import parent_classes
+from dask import dataframe as dd
+from pandas import dataframe as pdf
 
 
 def check(obj):
@@ -20,6 +22,8 @@ class Loader(parent_classes.Loader):
 
 
 def dump(obj, savedir):
+    if isinstance(obj, dd) or isinstance(obj, pdf):
+        obj.columns = obj.columns.asstype(str)  # parquet must have string column names
     obj.to_parquet(os.path.join(savedir, 'pandas_to_parquet.parquet'))
     compatibility.cloudpickle_fun(Loader(),
                                   os.path.join(savedir, 'Loader.pickle'))
