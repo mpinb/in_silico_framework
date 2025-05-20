89c68ae2a90ff4e5237a17f0e280fdc2e73abbe4
Bjorge Meulemeester <bjorge1997@hotmail.com>
2025-05-20 17:17:56 +0200

    Run mechanism compilation in correct environment so windows knows where to find `nrnivmodl`

--- mechanisms/l5pt/__init__.py
+++ mechanisms/l5pt/__init__.py
@@ -65,7 +65,7 @@ def _compile_mechanisms_at_path(path):
     Compile the mechanisms in the given path using nrnivmodl.
     This function is only needed if the mechanisms are not already compiled.
     """
-    subprocess.run(['nrnivmodl'], cwd=path, check=True)
+    subprocess.run(['nrnivmodl'], cwd=path, check=True, env=os.environ.copy())
 
 def compile_l5pt_mechanisms(force_recompile=False):
     """
