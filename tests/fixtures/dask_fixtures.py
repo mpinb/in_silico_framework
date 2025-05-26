e732ec034543e7822e4e80c9a6f3872a5cc1aca1
Bjorge Meulemeester <38312921+bgmeulem@users.noreply.github.com>
2025-05-26 17:08:12 +0200

    Lazily evaluate the dask client in fixture

--- tests/fixtures/dask_fixtures.py
+++ tests/fixtures/dask_fixtures.py
@@ -1,5 +1,6 @@
 import pytest, os, time, socket
 from distributed.comm.core import CommClosedError
+from tornado.iostream import StreamClosedError
 from dask.distributed import Client
 
 
@@ -8,30 +9,36 @@ def client(pytestconfig):
     """Fixture to create a Dask client for the tests.
     
     This fixture requests the dask_cluster to ensure it is launched.
-    For non-parent pytest workers, the `dask_scheduler` fixture will be None.
-    For this reason, we simply use the pytest config to connect to the singular scheduler.
+
+    The dask client is evaluated lazily. This helps in race conditions where the client
+    otherwise would try to connect before the Dask cluster is ready.
     """
     # Each pytest xdist worker will create its own Dask client
     # and connect to the same Dask cluster.
     # The tests should thus be thread-safe
-    ip = pytestconfig.getoption("dask_server_ip")
-    port = int(pytestconfig.getoption("dask_server_port"))
-    address = f"{ip}:{port}"
 
-    max_wait = 30  # seconds
-    interval = 1
-    start = time.time()
+    _client = None
+    
+    def get_client():
+        nonlocal _client
+        ip = pytestconfig.getoption("dask_server_ip")
+        port = int(pytestconfig.getoption("dask_server_port"))
+        address = f"{ip}:{port}"
+        max_wait = pytestconfig.getini("DASK_CLIENT_TIMEOUT")
+        last_exc = None
+        interval = 2
+        start = time.time()
+        while True:
+            try:
+                _client = Client(address, timeout=10)
+                break
+            except (OSError, TimeoutError, StreamClosedError, CommClosedError) as e:
+                last_exc = e
+                if time.time() - start > max_wait:
+                    raise RuntimeError(f"Could not connect to Dask scheduler at {address} within {max_wait} seconds") from last_exc
+                time.sleep(interval)
+        return _client
 
-    while True:
-        try:
-            client = Client(address, timeout=max_wait)
-            break
-        except (OSError, TimeoutError, CommClosedError, socket.error):
-            if time.time() - start > max_wait:
-                raise RuntimeError(
-                    f"Could not connect to Dask scheduler at {address} within {max_wait} seconds"
-                )
-            time.sleep(interval)
-
-    yield client
-    client.close()
+    yield get_client
+    if _client:
+        client.close()
