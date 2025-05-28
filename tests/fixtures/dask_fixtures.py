e338f7c80146d4a3cbce3cb41d264901ae11f78c
Bjorge Meulemeester <38312921+bgmeulem@users.noreply.github.com>
2025-05-28 18:51:50 +0200

    use `client.run` after all?

--- tests/fixtures/dask_fixtures.py
+++ tests/fixtures/dask_fixtures.py
@@ -44,9 +44,7 @@ def client(pytestconfig):
     client = Client(cluster)
     client.wait_for_workers(n_workers)
     # load mechanisms into NEURON namespace on whichever dask worker is assigned this test
-    f = client.submit(init_dask_workers)
-    f.result()  # wait for all workers to report back before moving on
-    # logger.info(f"Using Dask cluster with {n_workers} workers on port {scheduler_port}")
+    client.run(init_dask_workers)
     
     yield client
     client.close()
