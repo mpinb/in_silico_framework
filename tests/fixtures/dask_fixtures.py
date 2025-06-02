5c4303074b8c95445e46d3173887516344c8dbbf
Bjorge Meulemeester <38312921+bgmeulem@users.noreply.github.com>
2025-06-02 13:32:21 +0200

    Keep dask worker init in config file, not pytest config

--- tests/fixtures/dask_fixtures.py
+++ tests/fixtures/dask_fixtures.py
@@ -1,5 +1,4 @@
 import pytest, logging, socket, sys
-from distributed.diagnostics.plugin import SchedulerPlugin
 from dask.distributed import LocalCluster, Client
 logger = logging.getLogger("ISF").getChild(__name__)
 
@@ -9,40 +8,6 @@ def get_free_port():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(('', 0))
         return s.getsockname()[1]
-
-
-def init_dask_workers():
-    """Initialize Dask worker with ISF mechanisms loaded.
-    
-    This is wrapped in a function to avoid recursion issues.
-    Recursion issues may arise on windows because:
-
-    - Windows spawns processes using spawn() instead of fork()
-    - importing mechanisms.l5pt triggers load_mechanisms()
-    - Pickling the load_mechanisms function subsequently leads to recursion issues 
-
-    I don't fully understand why this happens, but wrapping it in a function seems to resolve the issue.
-
-    - Bjorge 2025-05-28
-    """
-    try:
-        from mechanisms.l5pt import load_mechanisms
-        load_mechanisms()
-    except Exception as e:
-        print("Worker mechanism load error:", e, file=sys.stderr)
-        raise
-
-
-def safe_init_dask_workers(client, n_retries=3):
-    workers = client.run_on_scheduler(lambda dask_scheduler: list(dask_scheduler.workers.keys()))
-    for worker in workers:
-        try:
-            future = client.submit(init_dask_workers, workers=[worker])
-            future.result()  # Wait for the result
-            break  # Exit retry loop if successful
-        except Exception as e:
-            logger.error("Failed to initialize Dask worker after %d retries: %s", n_retries, e)
-            raise e
 
 
 @pytest.fixture(scope="function")
@@ -60,8 +25,6 @@ def client(pytestconfig):
         silence_logs = False,
     )
     client = Client(cluster)
-    # client.wait_for_workers(n_workers)
-    # safe_init_dask_workers(client)
     
     yield client
     client.close()
