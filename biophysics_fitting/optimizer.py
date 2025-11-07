9ac9c0396a691b46543ae9862b49a296bcee55b5
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2025-11-07 12:11:08 +0100

    add deprecation notice

--- biophysics_fitting/optimizer.py
+++ biophysics_fitting/optimizer.py
@@ -389,8 +389,11 @@ def eaAlphaMuPlusLambdaCheckpoint(
         stats (deap.tools.Statistics): generation of statistics
         halloffame (deap.tools.HallOfFame): hall of fame
         cp_frequency (int): generations between checkpoints
-        cp_filename (DataBase or None): db_run, where the checkpoint is stored in [generation]_checkpoint. Was: path to checkpoint filename
+        db_run (DataBase or None): db where the checkpoint is stored in [generation]_checkpoint.
         continue_cp (bool): whether to continue
+
+    .. deprecated 0.5.0::
+       The argument ``cp_filename`` is deprecated. It has now been changed to ``db_run`` and ``continue_cp``.
     """
     # --- added by arco
     if db_run is not None:
