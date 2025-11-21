06201c4ea77f235dff64bb70d267087770df49b3
su-saka <suzeynepsaka@gmail.com>
2025-11-21 12:13:24 +0100

    add option to insert mechanisms with a positive linear slope

--- single_cell_parser/cell_parser.py
+++ single_cell_parser/cell_parser.py
@@ -431,7 +431,7 @@ class CellParser(object):
                                 h.pop_section()
 
             elif mech.spatial == 'linear':
-                ''' spatially linear distribution with negative slope'''
+                ''' spatially linear distribution with slope'''
                 maxDist = self.cell.max_distance(label)
                 #                set origin to 0 of first branch with this label
                 if label == 'Soma':
@@ -459,8 +459,11 @@ class CellParser(object):
                             dist = self.cell.distance_to_soma(sec, seg.x)
                             if relDistance:
                                 dist = dist / maxDist
-                            #rangeVarVal = mech[param]*(dist*slope + offset) 
-                            rangeVarVal = max(mech[param] * (dist * slope + 1),
+                            if slope > 0: # positive slope
+                                rangeVarVal = min(mech[param] * (dist * slope + 1),
+                                              mech[param] * offset)
+                            else: 
+                                rangeVarVal = max(mech[param] * (dist * slope + 1),
                                               mech[param] * offset)
                             s = param + '=' + str(rangeVarVal)
                             paramStrings.append(s)
