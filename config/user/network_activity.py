870db0affaf0ab1b7ebe78e06a4da81fd6745493
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2026-03-19 15:56:22 +0100

    Remove explicit activity data map (we use globstrings now)

--- config/user/network_activity.py
+++ config/user/network_activity.py
@@ -38,44 +38,6 @@ from pathlib import Path
 from getting_started import example_data_dir
 ACTIVITY_DATA_DIR = Path(example_data_dir) / "functional_constraints" / "evoked_activity"
 
-# -------------------- Activity data
-# For full control of the generation of network paramters, please use the NetworkParamBuilder instead.
-
-ACTIVITY_DATA_MAP = {
-    'A1':       ACTIVITY_DATA_DIR / "A1_stim.param",
-    'A2':       ACTIVITY_DATA_DIR / "A2_stim.param",
-    'A3':       ACTIVITY_DATA_DIR / "A3_stim.param",
-    'A4':       ACTIVITY_DATA_DIR / "A4_stim.param",
-    'Alpha':    ACTIVITY_DATA_DIR / "Alpha_stim.param",
-    'B1':       ACTIVITY_DATA_DIR / "B1_stim.param",
-    'B2':       ACTIVITY_DATA_DIR / "B2_stim.param",
-    'B3':       ACTIVITY_DATA_DIR / "B3_stim.param",
-    'B4':       ACTIVITY_DATA_DIR / "B4_stim.param",
-    'Beta':     ACTIVITY_DATA_DIR / "Beta_stim.param",
-    'C1':       ACTIVITY_DATA_DIR / "C1_stim.param",
-    'C2':       ACTIVITY_DATA_DIR / "C2_stim.param",
-    'C3':       ACTIVITY_DATA_DIR / "C3_stim.param",
-    'C4':       ACTIVITY_DATA_DIR / "C4_stim.param",
-    'D1':       ACTIVITY_DATA_DIR / "D1_stim.param",
-    'D2':       ACTIVITY_DATA_DIR / "D2_stim.param",
-    'D3':       ACTIVITY_DATA_DIR / "D3_stim.param",
-    'D4':       ACTIVITY_DATA_DIR / "D4_stim.param",
-    'Delta':    ACTIVITY_DATA_DIR / "Delta_stim.param",
-    'E1':       ACTIVITY_DATA_DIR / "E1_stim.param",
-    'E2':       ACTIVITY_DATA_DIR / "E2_stim.param",
-    'E3':       ACTIVITY_DATA_DIR / "E3_stim.param",
-    'E4':       ACTIVITY_DATA_DIR / "E4_stim.param",
-    'Gamma':    ACTIVITY_DATA_DIR / "Gamma_stim.param",
-}
-"""
-:ref:`activity_data_format` files containing empirical data on _in vivo_ PSTHs of all cell types considered in this project.
-These are used to generate :ref:`network_param_format` files.
-
-These files are used when passing a string instead of actual :ref:`activity_data_format` to 
-:meth:`~singlecell_input_mapper.network_param_from_tepmlate.build_network_param_from_template`.
-Those strings will be interpreted as a key that should map to activity data files defined here.
-"""
-
 NETWORK_PARAM_TEMPLATE_FN = Path(example_data_dir) / "functional_constraints" / "ongoing_activity" / "ongoing_activity_celltype_template_exc_conductances_fitted.param"
 """A template :ref:`network_parameters_format` file containing filled-in values for synapse dynamics and ongoing firing intervals.
 
