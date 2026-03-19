0e1f8b3fba41b3bb6bfa8e1dc0ad413a441682f4
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2026-03-19 16:38:12 +0100

    Update docs

--- singlecell_input_mapper/network_param_from_template.py
+++ singlecell_input_mapper/network_param_from_template.py
@@ -63,6 +63,11 @@ def build_network_param_from_template(
 ) -> NTParameterSet:
     """Build :ref:`network_parameters_format` from a template.
 
+    This function adds the following info:
+    
+    - Network embedding info (:ref:`syn_file_format`, :ref:`con_file_format` files and amount of connected cells per cell type)
+    - Activity data
+
     A :ref:`network_parameters_format` template is a partially built :ref:`network_parameters_format` object that already contains info on:
 
     - Synapse dynamics per cell type
@@ -93,6 +98,9 @@ def build_network_param_from_template(
         write_all_celltypes (bool): Whether to write out information of celltypes, even if they are not connected. Default: False.
         additional_activity_params (:class:`~single_cell_parser.parameters.NTParameterSet`):
             Additional network parameters to add.
+
+    See also:
+        :func:`~build_network_param_from_template` for a version that also adds activity data beyond ongoing activity.
 
     Returns:
         :class:`~single_cell_parser.parameters.NTParameterSet`: The :ref:`network_parameters_format`.
@@ -131,9 +139,7 @@ def build_network_param_from_template(
 
     template = build_parameters(filename=template_fn)
 
-    # TODO document this function expects celltypes of the form ct_area
     celltypes_anareas = read_nr_connected_cells_from_con(con_fn).index.values
-    celltypes = set([e.split("_")[0] for e in celltypes_anareas])
     anatomical_area = set([e.split("_")[-1] for e in celltypes_anareas])
     subcategorized_celltypes_map = {
         ct: [f"{ct}_{area}" for area in anatomical_area] 
@@ -150,6 +156,94 @@ def build_network_param_from_template(
     ).add_activity(
         activity_per_ct = activity_per_ct,
         additional_params = additional_activity_params,
+    ).network_parameters
+
+    if out_fn != None:
+        netp.save(out_fn)
+
+    return netp
+
+def build_ongoing_network_param_from_template(
+    template_fn: str | Path | None = None,
+    cell_nr_fn = None, # unused, but we keep for backwards compatibility
+    syn_fn: str | Path = None,
+    con_fn: str | Path = None,
+    out_fn: str | None = None,
+    write_all_celltypes: bool = False,
+    additional_activity_params: NTParameterSet | None = None,
+) -> NTParameterSet:
+    """Build :ref:`network_parameters_format` from a template.
+
+    This function adds the following info:
+    
+    - Network embedding info (:ref:`syn_file_format`, :ref:`con_file_format` files and amount of connected cells per cell type)
+    - Activity data
+
+    A :ref:`network_parameters_format` template is a partially built :ref:`network_parameters_format` object that already contains info on:
+
+    - Synapse dynamics per cell type
+    - Ongoing activity per cell type
+    - Any additional preconfigured activity parameters (e.g. "offset")
+
+    It is assumed that the cell types in the template have no subcategorization per anatomical area.
+    It is also asusmed that the network embedding and activity information **does** have cell types defined per anatomical area, separated by an underscore.
+
+    Example::
+
+        >>> template.network.keys()
+        ['type1', 'type2', ...]
+        >>> netp = build_network_param_from_template(template, ...)
+        >>> netp.keys()
+        ['type1_area1' ... 'type2_area4' ...]
+
+    Args:
+        template_fn (str | :class:`pathlib.Path`): The name of the template file. If not given, defaults to the one configured in the user settings. Default: None (user settings fallback).
+        syn_fn (str | :class:`pathlib.Path`): The name or Path to a :ref:`syn_file_format` file.
+        con_fn (str | :class:`pathlib.Path`): The name or Path to a :ref:`con_file_format` file.
+        out_fn (str): Where to save the :ref:`network_parameters_format` to. Default: None (don't save).
+        write_all_celltypes (bool): Whether to write out information of celltypes, even if they are not connected. Default: False.
+        additional_activity_params (:class:`~single_cell_parser.parameters.NTParameterSet`):
+            Additional network parameters to add.
+
+    Returns:
+        :class:`~single_cell_parser.parameters.NTParameterSet`: The :ref:`network_parameters_format`.
+
+    See also:
+        :func:`~build_network_param_from_template` for a version that also adds activity data beyond ongoing activity.
+
+
+    .. deprecated 0.6.0::
+       The :param:`cell_nr_fn` is deprecated and no longer needed. It may be removed in a future version.
+    """
+
+    if template_fn == None:
+        logger.info(f"No network parameter template passed. Falling back to the default: {NETWORK_PARAM_TEMPLATE_FN}")    
+        template_fn = NETWORK_PARAM_TEMPLATE_FN
+
+    if cell_nr_fn is not None:
+        logger.warning("The cell number filename is no longer needed, as all relevant info exists in the .con file Cell ID.")
+
+    if additional_activity_params == None and len(ADDITIONAL_NETWORK_ACTIVITY_PARAMS) > 0:
+        logger.info("No additional activity parameters passed, but the user config has them defined. Using the user-configured additional network parameters...")
+        additional_activity_params = NTParameterSet(ADDITIONAL_NETWORK_ACTIVITY_PARAMS)
+
+
+    template = build_parameters(filename=template_fn)
+
+    celltypes_anareas = read_nr_connected_cells_from_con(con_fn).index.values
+    anatomical_area = set([e.split("_")[-1] for e in celltypes_anareas])
+    subcategorized_celltypes_map = {
+        ct: [f"{ct}_{area}" for area in anatomical_area] 
+        for ct in template.network.keys()
+    }
+
+    netp = NetworkParamBuilder(
+        netp=template
+    ).subcategorize_celltypes(
+        cell_type_map = subcategorized_celltypes_map
+    ).add_network_embedding(
+        syn_fn = syn_fn,
+        con_fn = con_fn,
     ).network_parameters
 
     if out_fn != None:
