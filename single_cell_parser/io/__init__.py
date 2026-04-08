54a3c1954e3ca58f2cdd00385ca0a0ae9aa297ca
bjorge@framework <bjorge1997@hotmail.com>
2026-04-08 16:36:15 +0200

    Add `__init__` to `single_cell_parser.io` so it is a proper pacakge, and `rst` can reference it

---
+++ single_cell_parser/io/__init__.py
@@ -0,0 +1,22 @@
+# In Silico Framework
+# Copyright (C) 2025  Max Planck Institute for Neurobiology of Behavior - CAESAR
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Input/ouput operations for various file formats:
+
+- :ref:`activity_data_format` and associated files in :mod:`~activity`
+- Connectivity data such as :ref:`syn_file_format` and :ref:`con_file_format` files in :mod:`~connectivity`
+- AMIRA files such as `ScalarField` objects in :ref:`~amira`
+- Morphologies in :ref:`hoc_file_format` and :ref:`swc_file_format` in :mod:`~morphology`
+"""
